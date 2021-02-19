fcbd6b66c8019bc2c1930720df36f7ed048b9bca
taukir ahmmed <taukirahmed779@gmail.com>
2021-02-19 17:25:29 +0600

    debug true

--- mainblog/settings.py
+++ mainblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '470ftg*p96bhdzb*yh9$)^-312%$6xr&iyh^d_sxt8zm3s_a$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vast-ravine-94794.herokuapp.com/', 'localhost', '127.0.0.1']
 
